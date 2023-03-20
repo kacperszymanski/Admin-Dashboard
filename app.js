44114289c48fc0510085444fb0a03fc3bdfb0bb3
kacperszymanski <akacperszymanski@gmail.com>
2023-03-20 20:54:00 +0100

    add style

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function setTheme() {
   const root = document.documentElement;
-  const themeName = document.querySelector(".theme-name");
-  const newTheme = root.className === "dark" ? "light" : "dark";
+  const themeName = document.querySelector('.theme-name');
+  const newTheme = root.className === 'dark' ? 'light' : 'dark';
   root.className = newTheme;
   themeName.textContent = newTheme;
 }
 
-const themeToggle = document.querySelector(".theme-toggle");
-themeToggle.addEventListener("click", setTheme);
+const themeToggle = document.querySelector('.theme-toggle');
+themeToggle.addEventListener('click', setTheme);
 
 setTheme();
